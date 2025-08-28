0fc6bc98b9ecfa9ad15e9dff76387fca3bec8434
aireportstudio <projectwork9892@gmail.com>
2025-08-29 00:40:13 +0530

    301 & favicon related changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -199,7 +199,6 @@ export default function RootLayout({
         <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
         <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
         <link rel="manifest" href="/site.webmanifest" />
-        <link rel="canonical" href="https://www.aiprojectreport.com" />
       </head>
       <body className="font-sans antialiased">{children}</body>
     </html>
